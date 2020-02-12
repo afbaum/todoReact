8eb1a897c060f50d23664652427174fc0a4a630e
Amanda <amanda.baum01@gmail.com>
2020-02-11 21:29:44 -0500

    Updates

---
+++ precache-manifest.890695db04d98b4ea7ff1c5101704499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caca006d17baf38e75cc668c96bf42fc",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "7339de9ded2538822cac",
+    "url": "/todoReact/static/css/main.695f7729.chunk.css"
+  },
+  {
+    "revision": "1075d781ad416872ba61",
+    "url": "/todoReact/static/js/2.dae7de80.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todoReact/static/js/2.dae7de80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7339de9ded2538822cac",
+    "url": "/todoReact/static/js/main.599c3b14.chunk.js"
+  },
+  {
+    "revision": "4c36afd31f0936853ed0",
+    "url": "/todoReact/static/js/runtime-main.6c41d0c9.js"
+  }
+]);
